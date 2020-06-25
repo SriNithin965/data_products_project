1857e69b137518e2825d58d13ceb4033c05d7c54
SriNithin965 <srinithin00@gmail.com>
2020-06-25 17:07:57 +0530

    project_final

--- project/server.R
+++ project/server.R
@@ -1,74 +1,51 @@
 library(shiny)
 library(corrplot)
+library(lattice)
+library(ggplot2)
+library('e1071')
 library(rpart)
 library(rpart.plot)
 library(caret)
-library("gbm")
+library(gbm)
+library(randomForest)
 data(iris)
 shinyServer(function(input,output){
-
-  inTrain1 <- createDataPartition(y=iris$Species,p=0.5,list = FALSE)
-  training1 <- iris[inTrain1,]
-  testing1 <- iris[-inTrain1,]
-  model1 <- train(Species~.,data=training1,method = "rf")
-  pred1 <- reactive({pred <- predict(model1,testing1)
-    testing1$predRight <- pred ==testing1$Species
-    table(pred,testing1$Species)})
-  
-  iris2 <- subset(iris,select= c(Petal.Length,Petal.Width,Species))
-  inTrain2 <- createDataPartition(y=iris2$Species,p=0.6,list = FALSE)
-  training2 <- iris2[inTrain2,]
-  testing2 <- iris2[-inTrain2,]
-  model2 <- train(Species~.,data=training2,method = "rf")
-  pred2 <- reactive({pred <- predict(model2,testing2)
-   testing2$predRight <- pred ==testing2$Species
-   table(pred,testing2$Species)})
-    
-  iris_3 <- subset(iris,select = c(Sepal.Length,Sepal.Width,Species))
-  inTrain3 <- createDataPartition(y=iris_3$Species,p=0.5,list = FALSE)
-  training3 <- iris_3[inTrain3,]
-  testing3 <- iris_3[-inTrain3,]
-  model3 <- train(Species~.,data=training3,method = "rf")
-  pred3 <- reactive({pred <- predict(model3,testing3)
-    testing3$predRight <- pred ==testing3$Species
-    table(pred,testing3$Species)})
-    
-    output$plot1 <- renderPlot({
-      if(input$radio==3){
-      corrPlot <- cor(training1[, -length(names(training1))])
-      corrplot(corrPlot, method="color")}
-      else if(input$radio == 2){
-        corrPlot <- cor(training2[, -length(names(training2))])
-        corrplot(corrPlot, method="color") 
-      }
-      else{
-        corrPlot <- cor(training3[, -length(names(training3))])
-        corrplot(corrPlot, method="color")
-      }
+  model1 <-reactive({
+    if(input$radio1==1){
+    model <- train(Species~.,data=iris,method = "rf")
+  }
+  else if(input$radio1==2){
+    model <- train(Species~.,data=iris,method = "gbm")
+  }
+  else{
+    model <- train(Species~.,data=iris,method = "rpart")
+  }
+  }) 
+   output$plot1 <- renderPlot({
+      corrPlot <- cor(iris[, -length(names(iris))])
+      corrplot(corrPlot, method="color")
+})
+  pred <- reactive({ 
+    val <- input$pre
+    test <- iris[1:val,]
+    pred <- predict(model1(),test)
+   test$predRight <- pred ==test$Species
+   test
+  })
+    output$prediction <- renderPlot({
+        test <- pred()
+      qplot(Petal.Width,Petal.Length,colour=predRight,
+            data=test,main="predictions")
       })
-    output$plot2 <- renderPlot({
-      if(input$radio==3){
-        treeModel <- rpart(Species ~ ., data=training1, method="class")
-        prp(treeModel)}
-      else if(input$radio == 2){
-        treeModel <- rpart(Species ~ ., data=training2, method="class")
-        prp(treeModel) 
-      }
-      else{
-        treeModel <- rpart(Species ~ ., data=training3, method="class")
+    
+      output$plot2 <- renderPlot({
+        treeModel <- rpart(Species ~ ., data=iris, method="class")
         prp(treeModel)
-      }
     })
-    output$prediction <- renderTable({
-      if(input$radio==3){
-      pred1()
-      }
-      else if (input$radio == 2){
-        pred2()
-      }
-      else {
-        pred3()
-      }
-      })
+    output$plot3 <- renderPlot({
+      test <- pred()
+      qplot(Sepal.Width,Sepal.Length,colour=predRight,
+            data=test,main="predictions")
+    })
 })
 
--- project/ui.R
+++ project/ui.R
@@ -3,17 +3,18 @@ shinyUI(fluidPage(
     titlePanel("predict the species using the factor"),
     sidebarLayout(
         sidebarPanel(
-        radioButtons("radio", label = h3("prediction by factors"),
-                     choices = list( "Septals"=1,"petals"=2,"both"=3), 
-                     selected = 1)
-        ),
+        radioButtons("radio1", label = h3("prediction by which algrothim"),
+                     choices = list( "rf:random forest"=1,"gbm:multivarite regression"=2,"rpart:decision tree"=3), 
+                     selected = 1),
+        sliderInput("pre", "select the size of prediction:", min = 1, max = 150, value = 15)
+    ),
     mainPanel(
         h3("plot"),
         plotOutput("plot1"),
         h3("Decision tree"),
         plotOutput("plot2"),
         h3("prediction"),
-        tableOutput("prediction")
-        
+        plotOutput("prediction"),
+        plotOutput('plot3')
     ))
 ))
